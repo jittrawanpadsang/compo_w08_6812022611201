088320b06b8cb670199f99ae882ade603a2c7524
jittrawanpadsang <s6812022611201@email.kmutnb.ac.th>
2025-09-08 16:11:47 +0700

    Update coding08_1.2.c

--- coding08_1.2.c
+++ coding08_1.2.c
@@ -2,7 +2,7 @@
 
 int main(){
     for (int i = 2; i<= 10; i+=2) {
-        printf("Round %d\n" , i);
+        printf("%d\n" , i);
     }
 
     printf("End of loop\n");
@@ -10,4 +10,5 @@ int main(){
     
     
     
+
 }
