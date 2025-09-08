7b2a96c6fe5abc57ca0f9ae3a2bd75237a33f783
jittrawanpadsang <s6812022611201@email.kmutnb.ac.th>
2025-09-08 16:20:43 +0700

    Update coding08_1.2.c

--- coding08_1.2.c
+++ coding08_1.2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-    for (int i = 2; i<= 10; i+=2) {
+    for (int i = 2; i<= 10; i+=2) { ///ใช้for loopทำงานวนซ้ำจนครบ10รอบ แต่ละรอบเพิ่มขึ้นทีละ2
         printf("%d\n" , i);
     }
 
@@ -12,3 +12,4 @@ int main(){
     
 
 }
+
